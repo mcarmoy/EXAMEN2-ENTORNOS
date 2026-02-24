b75b1152def706a897b355547a99fac6cda961bb
Miguel Ángel Carrasco Moya <mcarmoy2401@g.educaand.es>
2026-02-24 11:59:20 +0100

    Bloque main comenzado para iniciar a debuggear

--- ejercicio.py
+++ ejercicio.py
@@ -4,4 +4,22 @@ ListaAgenda = [{"titulo": "Estudiar Python", "hecha": False},
 
 def agregar_tareas(tareas, titulo):
     tareas.append({"titulo": titulo, "hecha": False})
-    
+
+def marcar_hecha(tareas, titulo):
+    for i in tareas:
+        if i["titulo"] == titulo: 
+            i["hecha"] = True
+        print(f"No se ha encontrado el titulo {titulo}")
+
+
+
+
+
+if __name__ == "__main__":
+
+ ListaAgenda = [{"titulo": "Estudiar Python", "hecha": False}, 
+                {"titulo": "Hacer ejercicio", "hecha": True},
+                {"titulo": "Leer 10 páginas", "hecha": False}]
+
+
+ 
